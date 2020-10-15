7a5e9715f6b0048498472dbf46b657ba69104923
zerolzhy <44442503+zerolzhy@users.noreply.github.com>
2020-10-15 16:44:31 -0400

    Padding 0 for the clock

--- src/Clock/Clock.js
+++ src/Clock/Clock.js
@@ -4,11 +4,11 @@ const clock = (props) => {
     
     return (<div className="Clock">
         <h2>
-            <input type="text" value={props.hour} id="hour" onChange={props.changeHour} onFocus={props.focus} onKeyUp={props.enterUp} />
+            <input type="text" value={String(props.hour).padStart(2, '0')} id="hour" onChange={props.changeHour} onFocus={props.focus} onKeyUp={props.enterUp} />
             :
-            <input type="text" value={props.minutes} id="minutes" onChange={props.changeMin} onFocus={props.focus} onKeyUp={props.enterUp} />
+            <input type="text" value={String(props.minutes).padStart(2, '0')} id="minutes" onChange={props.changeMin} onFocus={props.focus} onKeyUp={props.enterUp} />
             :
-            <input type="text" value={props.seconds} id="second" onChange={props.changeSec} onFocus={props.focus} onKeyUp={props.enterUp} />
+            <input type="text" value={String(props.seconds).padStart(2, '0')} id="second" onChange={props.changeSec} onFocus={props.focus} onKeyUp={props.enterUp} />
             <select list="A_PM" value={props.apm} onChange={props.changeAPM} onFocus={props.focus} onKeyUp={props.enterUp}>
                 <option value="AM">AM</option>
                 <option value="PM">PM</option>
